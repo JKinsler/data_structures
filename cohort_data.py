523c0ceb473220404affa9a629850d7caba1c379
Your Name <you@example.com>
2020-01-10 16:13:54 -0800

    working code for 'sort_by_cohorts' in cohorts_data.py

--- cohort_data.py
+++ cohort_data.py
@@ -51,15 +51,44 @@ def sort_by_cohort(filename):
     [['Harry Potter', 'Mandy Brocklehurst', 'Ron Weasley', 'Oliver Wood', 'Colin Creevey', 'Cho Chang', 'Michael Corner', 'Draco Malfoy', 'Seamus Finnigan', 'Eddie Carmichael', 'Theodore Nott', 'Terence Higgs', 'Hermione Granger', 'Penelope Clearwater', 'Angelina Johnson', 'Dennis Creevey'], ['Neville Longbottom', 'Cedric Diggory', 'Pansy Parkinson', 'Anthony Goldstein', 'Padma Patil', 'Luna Lovegood', 'Eleanor Branstone', 'Lee Jordan', 'Marietta Edgecombe', 'Andrew Kirke', 'Ginny Weasley', 'Mary Macdonald', 'Blaise Zabini', 'Natalie McDonald', 'Adrian Pucey', 'Hannah Abbott', 'Graham Pritchard', 'Susan Bones', 'Roger Davies', 'Owen Cauldwell'], ['Laura Madley', 'Orla Quirke', 'Parvati Patil', 'Eloise Midgeon', 'Zacharias Smith', 'Cormac McLaggen', 'Lisa Turpin', 'Demelza Robins', 'Ernie Macmillan', 'Millicent Bullstrode', 'Percy Weasley', 'Jimmy Peakes', 'Justin Finch-Fletchley', 'Miles Bletchley', 'Malcolm Baddock'], ['Marcus Belby', 'Euan Abercrombie', 'Vincent Crabbe', 'Ritchie Coote', 'Katie Bell', 'Terry Boot', 'Lavender Brown', 'Gregory Goyle', 'Marcus Flint', 'Dean Thomas', 'Jack Sloper', 'Rose Zeller', 'Stewart Ackerley', 'Fred Weasley', 'George Weasley', 'Romilda Vane', 'Alicia Spinnet', 'Kevin Whitby'], ['Friendly Friar', 'Grey Lady', 'Nearly Headless Nick', 'Bloody Baron']]
     """
 
+    house_data = open(filename)
+
     all_students = []
     winter_16 = []
     spring_16 = []
     summer_16 = []
     fall_15 = []
     ghosts = []
+    blanks = []
 
-    # Code goes here
+    for line in house_data:
+        #cleaning white space
+        person_info = line.rstrip()
+#       #parsing the data
+        person_info = line.split('|')
+#         # print(person_info)
+        #print(person_info)
+        
+        cohort = person_info[4]
+        new_cohort = cohort.rstrip()
+        #print(new_cohort)
 
+        if new_cohort == "":
+            blanks.append(person_info[0] + " " + person_info[1])
+        if new_cohort == "Winter 2016":
+            winter_16.append(person_info[0] + " " + person_info[1])
+        if new_cohort == "Spring 2016":
+            spring_16.append(person_info[0] + " " + person_info[1])
+        if new_cohort == "Summer 2016":
+            summer_16.append(person_info[0] + " " + person_info[1])
+        if new_cohort == "Fall 2015":
+            fall_15.append(person_info[0] + " " + person_info[1])
+        if new_cohort == "G":
+            ghosts.append(person_info[0] + " " + person_info[1])
+            #print(ghosts)
+    
+    all_students = [fall_15, winter_16, spring_16, summer_16, ghosts]  
+    #print(all_students)
     return all_students
 
 
